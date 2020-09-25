b12d032e68bf9d434bfb58232f63a990e81ee77a
Hyogeun Wang <w@endless9.com>
2020-09-25 14:08:08 +0900

    폴더 경로 임시 처리

--- src/sync.ts
+++ src/sync.ts
@@ -27,13 +27,19 @@ async function sync() {
   try {
     fs.writeFileSync(OUTPUT_PATH, m3u);
     console.log('- synced at ' + new Date());
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
 }
 
 function item2m3u(item: any): string {
   const entities = new Html5Entities();
+
+  // 파일경로 강제변경(5호점 대응)
+  if (item.path) {
+    item.path = item.path.replace('D:/', '/Volumes/Seagate/');
+  }
+
   const str =
     `#EXTVDJ:<filesize>0</filesize><artist>${entities.encode(item.artist)}</artist><title>${entities.encode(
       item.title
